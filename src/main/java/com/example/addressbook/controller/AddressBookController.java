aa2e34d4700acfba889cb38838883afc50846c10
Barnamwork <barnamwork@gmail.com>
2026-04-01 14:54:42 +0530

    [Barnam] UC2: Implemented REST APIs using Controller

--- src/main/java/com/example/addressbook/controller/AddressBookController.java
+++ src/main/java/com/example/addressbook/controller/AddressBookController.java
@@ -1,13 +1,34 @@
 package com.example.addressbook.controller;
 
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressBookController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Address Book Running";
+    private List<Map<String, String>> list = new ArrayList<>();
+
+    @GetMapping("/get")
+    public List<Map<String, String>> getAll() {
+        return list;
+    }
+
+    @PostMapping("/create")
+    public Map<String, String> create(@RequestBody Map<String, String> data) {
+        list.add(data);
+        return data;
+    }
+
+    @PutMapping("/update/{id}")
+    public Map<String, String> update(@PathVariable int id, @RequestBody Map<String, String> data) {
+        list.set(id, data);
+        return data;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        list.remove(id);
+        return "Deleted";
     }
 }
